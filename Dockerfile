ae8211ceeda953cf0ede1f30ce2e10b8d2cf94f4
eri-black <109592236+eri-black@users.noreply.github.com>
2022-08-18 14:35:01 +0700

    env

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM ubuntu:18.04
 
-RUN apt update && apt-get install -y openssh-server 
-RUN apt-get install -y ttylog
+RUN apt update && apt-get install -y ttylog
--- start.sh
+++ start.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-cat /opt/logs/logs >> /opt/logs/logs_old
-echo "" > /opt/logs/logs
-
-service ssh restart
-useradd -m -s /bin/bash tester
-echo tester:tester | chpasswd
+cat /opt/logs/$1 >> /opt/logs/$1_old
+echo "" > /opt/logs/$1
 
 echo "admin" > /dev/ttyUSB0
 echo "password" > /dev/ttyUSB0
 
-# while read -r line; do echo "$line" >> /opt/logs/logs; done < /dev/ttyUSB0
-ttylog -f -d /dev/ttyUSB0 > /opt/logs/logs
+ttylog -f -d /dev/ttyUSB0 > /opt/logs/$1
 
 #tail -f /dev/null

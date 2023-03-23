a7791e1e2b656436748634212ff4ee4791f0c38c
Ненашева Виктория Алексеевна <viktoriya.nenasheva@eltex.loc>
2023-03-23 09:19:54 +0700

    ver3

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
-FROM ubuntu:18.04
+FROM ubuntu
 
-RUN apt update && apt-get install -y telnet ser2net python3 python3-pip
-COPY ./crash-logs-docker/slsnif-0-4-4.deb /opt/
-RUN dpkg -i /opt/slsnif-0-4-4.deb
-RUN pip3 install pexpect flask
+RUN apt update && apt-get install -y telnet ser2net
--- crash-logs-docker/start.sh
+++ crash-logs-docker/start.sh
@@ -3,9 +3,8 @@
 cat /opt/logs/$1 >> /opt/logs/$1_old
 echo "" > /opt/logs/$1
 
-    slsnif -n -i /opt/logs/$1 -s 115200 /dev/ttyUSB0 -u > /dev/null 2>&1 &
+
 #    ser2net -C "TRACEFILE:tb0:/opt/logs/$1" tb=tb0
-    ser2net -n -C "51001:telnet:0:/dev/pts/0:115200 8DATABITS NONE 1STOPBIT" > /dev/null 2>&1 &
-    python3 /opt/app.py > /dev/null 2>&1 & 
+#    ser2net -n -C "51001:telnet:0:/dev/pts/0:115200 8DATABITS NONE 1STOPBIT" > /dev/null 2>&1 &
 
 tail -f /dev/null
